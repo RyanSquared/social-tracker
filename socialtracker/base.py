5fcd2bd82473bcdf00c9c60d43774d8a0ad12558
RyanSquared <vandor2012@gmail.com>
2017-11-21 11:15:02 -0600

    socialtracker/base: Add icon loader and cache

--- socialtracker/base.py
+++ socialtracker/base.py
@@ -1,16 +1,43 @@
 "Base class for socialTrackers."
+import collections
+import requests
 
 
 class SocialTracker(object):
     """Implementation class for all socialTrackers."""
+    # Store the last 5 icons downloaded
+    icons = collections.deque(maxlen=5)
 
     def __init__(self, tags=None):
+        self.posts_cache = []
         self.tags = tags if tags is not None else []
 
     def get_posts(self):
         """Return a generator to yield posts from an API."""
         raise NotImplementedError("base class", SocialTracker)
 
-    def render_icon(self, post):
-        """Create an icon from a post/URL and put it into a post"""
-        raise NotImplementedError("base class", SocialTracker)
+    @staticmethod
+    def extend_icon_cache(length):
+        "Extend the length of the icon cache (default 5 icons)."
+        old_icons = SocialTracker.icons
+        SocialTracker.icons = collections.deque(maxlen=length)
+        SocialTracker.icons.extendleft(old_icons)
+
+    @staticmethod
+    def render_icon(url, filename):
+        """Pull an icon from `url` and place the output in `filename`."""
+        for number, icon in enumerate(SocialTracker.icons):
+            if icon[0] == url:
+                # percolate up to implement priority
+                if number > 1:
+                    # but don't just cycle between the first two values
+                    SocialTracker.icons.remove(icon)
+                    SocialTracker.icons.appendleft(icon)
+                image = icon[1]
+                break
+        else:
+            image = requests.get(url).content
+            SocialTracker.icons.appendleft((url, image))
+        with open(filename, "wb") as handler:
+            handler.write(image)
+        return filename
