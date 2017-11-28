bfe2e87242f598968e65103b1cd459dc938980ed
RyanSquared <vandor2012@gmail.com>
2017-11-28 16:10:25 -0600

    setup: Add Arrow and dependency

--- setup.py
+++ setup.py
@@ -5,4 +5,4 @@ setup(
     name='socialTracker',
     version='0.1.0',
     packages=['socialtracker'],
-    install_requires=['twitter', 'requests'])
+    install_requires=['twitter', 'requests', 'arrow', 'backports.functools_lru_cache'])
