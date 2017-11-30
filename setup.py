917126be4fd71a14b6332fe675c06e62a7853765
RyanSquared <vandor2012@gmail.com>
2017-11-30 15:27:36 -0600

    setup: Add pillow

--- setup.py
+++ setup.py
@@ -7,4 +7,4 @@ setup(
     packages=['socialtracker'],
     install_requires=[
         'twitter', 'requests', 'arrow', 'backports.functools_lru_cache',
-        'unidecode'])
+        'unidecode', 'pillow'])
