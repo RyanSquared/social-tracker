e86604e066a97c72d3a0e45cef2061fecdc99986
RyanSquared <vandor2012@gmail.com>
2017-11-30 11:54:13 -0600

    setup: Add unidecode and rearrange

--- setup.py
+++ setup.py
@@ -5,4 +5,6 @@ setup(
     name='socialTracker',
     version='0.1.0',
     packages=['socialtracker'],
-    install_requires=['twitter', 'requests', 'arrow', 'backports.functools_lru_cache'])
+    install_requires=[
+        'twitter', 'requests', 'arrow', 'backports.functools_lru_cache',
+        'unidecode'])
