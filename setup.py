39d09a121c292c005a88c31f86f01e3af6f9cf21
RyanSquared <vandor2012@gmail.com>
2020-04-14 04:26:39 -0500

    setup: pin Pillow version, update ST version

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from distutils.core import setup
 
 setup(
     name='socialTracker',
-    version='0.1.0',
+    version='0.2.0',
     packages=['socialtracker'],
     install_requires=[
-        'twitter', 'requests', 'backports.functools_lru_cache', 'pillow',
-        'requests-oauthlib'])
+        'twitter', 'requests', 'backports.functools_lru_cache',
+        'pillow==5.4.1', 'requests-oauthlib'])
